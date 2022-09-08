7fd921cfa3c489a1cec34a23af570f86b3185968
AlvaroLuqmanW <99391051+AlvaroLuqmanW@users.noreply.github.com>
2022-09-08 13:25:17 +0700

    Update models.py

--- example_app/models.py
+++ example_app/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-class BarangWishlist(models.Model):
-    nama_barang = models.CharField(max_length=50)
-    harga_barang = models.IntegerField()
-    deskripsi = models.TextField()
+
